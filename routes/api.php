1c2abb5ebddacf1eb528c9c356a879e25e646897
pat56-hp <patrickkouassi7@gmail.com>
2025-07-17 18:36:34 +0000

    Ajout des routes Crud & update du status des utilisateurs

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+/*************************** Routes d'authentification ****************************   */
 Route::prefix('auth')->group(function(){
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
+});
+
+/*************************** Routes des utilisateurs ****************************   */
+Route::middleware(['auth:api'])->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::get('/change-status/{id}', [UserController::class, 'changeStatus']);
 });

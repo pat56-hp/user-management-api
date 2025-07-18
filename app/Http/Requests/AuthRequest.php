eba7aaf4f35972a71f0f8b91ec486a713125221e
pat56-hp <patrickkouassi7@gmail.com>
2025-07-18 15:49:16 +0000

    Fix: Modification de la structure AuthRequest pour retourner les erreurs en Json

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -3,7 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Contracts\Validation\Validator;
 class AuthRequest extends FormRequest
 {
     /**
@@ -26,4 +28,20 @@ class AuthRequest extends FormRequest
             'password' => 'required|string|min:8',
         ];
     }
+
+    /**
+     * Désactive la redirection
+     *
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Oups, une erreur dans le formulaire',
+                'data' => $validator->errors()
+            ], Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    } 
 }
